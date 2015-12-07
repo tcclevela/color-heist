8c19a4ea741ac8d0e47c39b4a04a58a36a3ce9c7
Teddy Cleveland <tcclevela@gmail.com>
2015-12-07 12:30:01 -0500

    bower to load deps, gulp to do the rest

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+// Include Gulp
+var gulp = require('gulp');
+var mainBowerFiles = require('gulp-main-bower-files');
+var uglify = require("gulp-uglify")
+var filter = require("gulp-filter")
+var concat = require("gulp-concat")
+var merge = require('merge-stream');
+// Define default destination folder
+var dest = 'dist/';
+var jsFiles = "src/color-heist.js"
+
+gulp.task('uglify', function(){
+
+    var bowerComponents = gulp.src('./bower.json')
+        .pipe(mainBowerFiles())
+    var source = gulp.src("./src/color-heist.js")
+    	
+    return merge(source, bowerComponents)
+    	.pipe(uglify())
+    	.pipe(concat("color-heist-min.js"))
+    	.pipe(gulp.dest(dest))
+
+
+});
